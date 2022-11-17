978a2d3ff10c56a1e6a24bad8579f5bc28376370
nilesh0901 <84271025+nilesh0901@users.noreply.github.com>
2022-11-18 03:30:01 +0530

    Update util.py

--- src/server/util.py
+++ src/server/util.py
@@ -5,7 +5,9 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
+
+def get_estimated_price(location, sqft, bhk, bath):
+    load_saved_artifacts()
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -18,22 +20,25 @@ def get_estimated_price(location,sqft,bhk,bath):
         x[loc_index] = 1
     return __model.predict([x])[0]
 
-def get_location_names():
-    return __locations
 
 def load_saved_artifacts():
-    print("Loading saved artifacts ..... start")
+    # print("Loading saved artifacts ..... start")
     global __data_columns
     global __locations
     global __model
-    with open("./artifacts/columns.json",'r') as f:
+    with open("./artifacts/columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
-    with open("./artifacts/House Price Prediction Project.pickle","rb") as f:
+    with open("./artifacts/House Price Prediction Project.pickle", "rb") as f:
         __model = pickle.load(f)
-    print("loading saved artifacts ...... Done")
+    # print("loading saved artifacts ...... Done")
+
+
+def get_location_names():
+    load_saved_artifacts()
+    return __locations
+
 
 if __name__ == "__main__":
-    load_saved_artifacts()
     print(get_location_names())
